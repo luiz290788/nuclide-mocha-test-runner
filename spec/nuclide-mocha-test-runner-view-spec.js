63429b8cce8905f888a393ea40ce3078e78b337e
Luiz Guilherme Pereira <luiz290788@gmail.com>
2016-04-21 13:27:22 -0300

    initial commit

---
+++ spec/nuclide-mocha-test-runner-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NuclideMochaTestRunnerView from '../lib/nuclide-mocha-test-runner-view';
+
+describe('NuclideMochaTestRunnerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
